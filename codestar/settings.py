9445b03952e3fc8cd448e33b3aa1b32a664bd976
David Clamp <dclamp@yahoo.com>
2025-12-18 16:35:54 +0000

    readies code for deploy

--- codestar/settings.py
+++ codestar/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ukv79ti__cn=bc7b1o_g9try#5mnqx@25+_!u)03l-cpaa&1#m'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [,'.herokuapp.com']
 
 
 # Application definition
