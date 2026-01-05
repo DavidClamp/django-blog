181f184d057424415e8d4ca20fbaedffc1726292
David Clamp <dclamp@yahoo.com>
2026-01-05 10:14:42 +0000

    stil...

--- codestar/settings.py
+++ codestar/settings.py
@@ -73,9 +73,9 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-            ],
+            ]
         }
-    },
+    }
 ]
 
 WSGI_APPLICATION = 'codestar.wsgi.application'
