e1c062ecb9d64241f662285a9b338970ae5ddafc
David Clamp <dclamp@yahoo.com>
2026-01-06 16:19:38 +0000

    view1 contd

--- blog/views.py
+++ blog/views.py
@@ -2,12 +2,10 @@ from django.shortcuts import render
 from django.views import generic
 from .models import Post
 # Create your views here.
-# from django.http import HttpResponse
 
-# def my_blog(request):
-#     return HttpResponse("Hello, blog!")
+
+
 class PostList(generic.ListView):
-    # model = Post
-    queryset = Post.objects.all()
-    template_name = "post_list.html"
+    queryset = Post.objects.filter(status=1)
+    # template_name = "post_list.html"
     
